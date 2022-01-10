dc419589c8e407715b12c9973c548ec0b809d003
sinhashlok <shlokjp@gmail.com>
2022-01-10 22:38:28 +0530

    practice

---
+++ A/Plus_One_on_Subset.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n; 
+        cin >> n;
+
+        int arr[n];
+        for(int i = 0; i < n; i++)
+            cin >> arr[i];
+        
+        cout << *max_element(arr, arr + n) - *min_element(arr, arr + n) << endl;
+    }
+
+    return 0;
+}
