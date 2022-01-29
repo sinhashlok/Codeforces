4ae64adec06a0f07b8eadfc909bfbfa5acea6900
sinhashlok <shlokjp@gmail.com>
2022-01-29 21:01:33 +0530

    practice

---
+++ A/Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+const int N=1e6;
+bool arr[N+5];
+
+void seive(){
+arr[0]=1;
+arr[1]=1;
+for(int i=2;i<=N;i++){
+	if(arr[i])continue;
+	for(long long j=(long long)i*i;j<=N;j+=i){
+		arr[j]=1;
+	}
+}
+
+}
+int main() {
+	seive();
+	int n ;
+	cin>>n;
+	int value1=(n>>1),value2=n-value1;
+	while(arr[value1]==0||arr[value2]==0){
+		++value1;
+		value2=n-value1;
+	}
+	cout<<value1<<" "<<value2;
+	return 0;
+}
